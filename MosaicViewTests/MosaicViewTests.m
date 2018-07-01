785fc8381f41c0b7967d94b76b1c4fb11d83b505
Superman <703353105@qq.com>
2018-07-01 20:21:15 +0800

    Initial Commit

---
+++ MosaicViewTests/MosaicViewTests.m
@@ -0,0 +1,39 @@
+//
+//  MosaicViewTests.m
+//  MosaicViewTests
+//
+//  Created by Superman on 2018/7/1.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MosaicViewTests : XCTestCase
+
+@end
+
+@implementation MosaicViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
